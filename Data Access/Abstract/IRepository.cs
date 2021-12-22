026f14833f362d966e4f264d76b7089d0d8fcce8
BEYZA ERDEM <192803045@ogr.cbu.edu.tr>
2021-12-22 14:35:20 +0300

    added repositories

--- Data Access/Abstract/IRepository.cs
+++ Data Access/Abstract/IRepository.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access.Abstract
 {
-    public interface IRepository
+    public interface IRepository<T>
     {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T type);
+        void Update(T type);
+        void Delete(T type);
     }
 }
--- Data Access/Concrete/Repositories/GenericRepository.cs
+++ Data Access/Concrete/Repositories/GenericRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Data_Access.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Access.Concrete.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IRepository<T> where T : class
     {
+        public Context context = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+
+        public void Add(T type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T type)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
