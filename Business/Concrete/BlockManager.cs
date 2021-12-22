8a62ca2637702ace0abbf19f78863770eb06ce54
sude-1 <69909424+sude-1@users.noreply.github.com>
2021-12-22 15:40:10 +0300

    block added

---
+++ Business/Concrete/BlockManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Data_Access.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class BlockManager : IBlockService
+    {
+        IBlockDal _blockDal;
+
+        public BlockManager(IBlockDal blockDal)
+        {
+            _blockDal = blockDal;
+        }
+
+        public void Add(Block block)
+        {
+            _blockDal.Add(block);
+        }
+
+        public void Delete(Block block)
+        {
+            _blockDal.Delete(block);
+        }
+
+        public List<Block> GetAll()
+        {
+            return _blockDal.GetAll();
+        }
+
+        public Block GetById(int id)
+        {
+            return _blockDal.Get(b => b.BlockId == id);
+        }
+
+        public void Update(Block block)
+        {
+            _blockDal.Update(block);
+        }
+    }
+}
