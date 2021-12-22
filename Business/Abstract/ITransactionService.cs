9c5dc86c6bd58ee05871e21f60358af487604a11
sude-1 <69909424+sude-1@users.noreply.github.com>
2021-12-22 15:54:01 +0300

    Transaction added

---
+++ Business/Abstract/ITransactionService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ITransactionService
+    {
+        List<Transaction> GetAll();
+        Transaction GetById(int id);
+        void Add(Transaction transaction);
+        void Update(Transaction transaction);
+        void Delete(Transaction transaction);
+    }
+}
---
+++ Business/Concrete/TransactionManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Data_Access.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class TransactionManager : ITransactionService
+    {
+        ITransactionDal _transactionDal;
+
+        public TransactionManager(ITransactionDal transactionDal)
+        {
+            _transactionDal = transactionDal;
+        }
+
+        public void Add(Transaction transaction)
+        {
+            _transactionDal.Add(transaction);
+        }
+
+        public void Delete(Transaction transaction)
+        {
+            _transactionDal.Delete(transaction);
+        }
+
+        public List<Transaction> GetAll()
+        {
+            return _transactionDal.GetAll();
+        }
+
+        public Transaction GetById(int id)
+        {
+            return _transactionDal.Get(t => t.TransactionId == id);
+        }
+
+        public void Update(Transaction transaction)
+        {
+            _transactionDal.Update(transaction);
+        }
+    }
+}
