deda76b7cb200bc521e6ec72b75c367c1ab11b35
Salih <salihozkara00@gmail.com>
2021-12-24 17:59:21 +0300

    update

--- Business/Utilities/Helpers/NodeJsAPIHelper.cs
+++ Business/Utilities/Helpers/NodeJsAPIHelper.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Entities.Concrete;
-using Newtonsoft.Json;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using Entities.Dto;
 
